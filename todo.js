2cdc5d49289a8259c1b2eade6a85f76a76ca66d2
Enrico Carruba <carrubaenrico@gmail.com>
2021-10-07 16:17:48 +0200

    add radio buttons for filtering

--- todo.js
+++ todo.js
@@ -1,9 +1,22 @@
 // Leeres, initiales Array
 let list = [];
+let doneList = [];
 
 // EventListener für Button
 let button = document.querySelector("#add-button");
 button.addEventListener("click", addToList);
+
+// Eventlistener für Radio Button Show Done
+let showDoneButton = document.querySelector("#show-done");
+showDoneButton.addEventListener("change", handleDoneEvent);
+
+// EventListener für Radio Button Show open
+let showOpenButton = document.querySelector("#show-open");
+showOpenButton.addEventListener("change", handleOpenEvent);
+
+// EventListener für Radio Button Show all
+let showAllButton = document.querySelector("#show-all");
+showAllButton.addEventListener("change", handleAllEvent);
 
 // Funktionen
 function addToList() {
@@ -37,7 +50,68 @@ function handleChangeEvent(e) {
   let li = checkbox.parentElement;
   if (checkbox.checked) {
     li.style.textDecoration = "line-through";
+    doneList.push(li.innerText.trim());
+    list.pop(li.innerText.trim());
   } else {
     li.style.textDecoration = "none";
+  }
+}
+
+function handleDoneEvent() {
+  // Alle Einträge aus der DOM Liste entfernen
+  removeChilds();
+  // Alle Einträge aus dem Done Array anzeigen
+  for (let i = 0; i < doneList.length; i++) {
+    let newLi = document.createElement("li");
+    newLi.innerText = doneList[i];
+    let listEl = document.querySelector("#list");
+    listEl.appendChild(newLi);
+  }
+  console.log(doneList);
+
+  // alle anderen Radio Buttons unchecken
+  showOpenButton.checked = false;
+  showAllButton.checked = false;
+}
+
+function handleOpenEvent() {
+  removeChilds();
+  for (let i = 0; i < list.length; i++) {
+    let newLi = document.createElement("li");
+    newLi.innerText = list[i];
+    let listEl = document.querySelector("#list");
+    listEl.appendChild(newLi);
+  }
+  console.log(list);
+  // alle anderen Radio Buttons unchecken
+  showAllButton.checked = false;
+  showDoneButton.checked = false;
+}
+
+function handleAllEvent() {
+  removeChilds();
+  for (let i = 0; i < doneList.length; i++) {
+    let newLi = document.createElement("li");
+    newLi.innerText = doneList[i];
+    let listEl = document.querySelector("#list");
+    listEl.appendChild(newLi);
+  }
+  for (let i = 0; i < list.length; i++) {
+    let newLi = document.createElement("li");
+    newLi.innerText = list[i];
+    let listEl = document.querySelector("#list");
+    listEl.appendChild(newLi);
+  }
+  console.log(list);
+  console.log(doneList);
+  // alle anderen Radio Buttons unchecken
+  showDoneButton.checked = false;
+  showOpenButton.checked = false;
+}
+
+function removeChilds() {
+  let removed = document.querySelector("#list");
+  while (removed.firstChild) {
+    removed.removeChild(removed.firstChild);
   }
 }
